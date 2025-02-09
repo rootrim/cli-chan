a5dc15f369fd043524c596d65f1b8bdcc77e5f64
Linium <bltyusuf03@gmail.com>
2025-02-09 20:23:33 +0300

    Update README.md
    added lock file
    
    added a jojo referancfe
    
    added dependencies
    
    fixed dependencies
    
    added nix support
    
    added flake.lock
    
    changes
    
    removed default.nix and added it directly in flake.nix

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "cli-chan: A simple CLI chatting application for the ones who live in terminal";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ rust-overlay.overlays.default ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+        rustToolchain = pkgs.rust-bin.stable.latest.default;
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "cli-chan";
+          version = "0.1.0";
+
+          src = ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+          nativeBuildInputs = with pkgs; [ pkg-config ];
+          buildInputs = with pkgs; [
+            pkg-config
+            openssl
+          ];
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            rustToolchain
+            pkgs.pkg-config
+          ];
+        };
+      }
+    );
+}
