1cde5ddd54725477cead24011e8709214086dd76
trim <omuaz01@gmail.com>
2026-07-08 11:57:28 +0300

    flake update

--- flake.nix
+++ flake.nix
@@ -1,54 +1,39 @@
 {
-  description = "cli-chan: A simple CLI chatting application for the ones who live in terminal";
+  description = "A cli AI anime girl app for those who live in terminal.";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    naersk.url = "github:nix-community/naersk";
+    naersk.inputs.nixpkgs.follows = "nixpkgs";
+    naersk.inputs.fenix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-      rust-overlay,
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        overlays = [ rust-overlay.overlays.default ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
-
-        rustToolchain = pkgs.rust-bin.stable.latest.default;
-      in
-      {
-        packages.default = pkgs.rustPlatform.buildRustPackage {
+  outputs = inputs @ {
+    flake-parts,
+    naersk,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      perSystem = {pkgs, ...}: let
+        naersk' = pkgs.callPackage naersk {};
+      in {
+        packages.default = naersk'.buildPackage {
           pname = "cli-chan";
           version = "0.1.0";
-
           src = ./.;
 
-          cargoLock = {
-            lockFile = ./Cargo.lock;
-          };
-
-          nativeBuildInputs = with pkgs; [ pkg-config ];
-          buildInputs = with pkgs; [
-            pkg-config
-            openssl
-          ];
+          nativeBuildInputs = with pkgs; [pkg-config];
         };
 
         devShells.default = pkgs.mkShell {
-          buildInputs = [
-            rustToolchain
-            pkgs.pkg-config
-            pkgs.openssl
+          nativeBuildInputs = with pkgs; [
+            rust-analyzer
+            clippy
+            pkg-config
           ];
         };
-      }
-    );
+      };
+    };
 }
