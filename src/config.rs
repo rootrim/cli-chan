1082f93582879f0c45009ae9b01882d10240fd5b
trim <omuaz01@gmail.com>
2026-07-16 14:16:26 +0300

    add tests for personality parsing

--- src/config.rs
+++ src/config.rs
@@ -25,9 +25,10 @@ impl Personality {
                 _ => {
                     let mut hobbie_string = String::new();
                     hobbie_string.push_str("Your hobbies are ");
-                    for hobbie in hobbies {
-                        hobbie_string.push_str(&format!("{hobbie}, "))
+                    for hobbie in &hobbies[..hobbies.len().saturating_sub(1)] {
+                        hobbie_string.push_str(&format!("{hobbie}, "));
                     }
+                    hobbie_string.push_str(hobbies.last().unwrap());
                     summary.push_str(&format!(" {hobbie_string}."));
                 }
             }
@@ -42,7 +43,7 @@ mod test {
     use super::*;
 
     #[test]
-    fn personality_summaring() {
+    fn personality_summaring_single_hobbie() {
         let personlaity_text =
             "You are Kuro, a jaded terminal spirit who has lived inside command lines for decades."
                 .to_string();
@@ -53,5 +54,37 @@ mod test {
         };
 
         assert_eq!(personality.summary(), "You are Kuro, a jaded terminal spirit who has lived inside command lines for decades. Your hobbie is coding.".to_string());
+    }
+
+    #[test]
+    fn personality_summaring_multiple_hobbies() {
+        let personlaity_text =
+            "You are Kuro, a jaded terminal spirit who has lived inside command lines for decades."
+                .to_string();
+
+        let personality = Personality {
+            personality: personlaity_text,
+            hobbies: Some(vec!["coding".into(), "hacking".into()]),
+        };
+
+        assert_eq!(personality.summary(), "You are Kuro, a jaded terminal spirit who has lived inside command lines for decades. Your hobbies are coding, hacking.".to_string());
+    }
+
+    #[test]
+    fn personality_summaring_no_hobbies() {
+        let personlaity_text =
+            "You are Kuro, a jaded terminal spirit who has lived inside command lines for decades."
+                .to_string();
+
+        let personality = Personality {
+            personality: personlaity_text,
+            hobbies: None,
+        };
+
+        assert_eq!(
+            personality.summary(),
+            "You are Kuro, a jaded terminal spirit who has lived inside command lines for decades."
+                .to_string()
+        );
     }
 }
